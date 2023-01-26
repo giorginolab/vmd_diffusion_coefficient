a4ec010f616b7b346bb29c6ea64ba3adcb05accd
Toni <toni.giorgino@gmail.com>
2023-01-26 15:49:17 +0100

    rename global variable to avoid clashes

--- load.tcl
+++ load.tcl
@@ -1,13 +1,13 @@
 # Startup helper.
 
 
-set here [file dirname [file normalize [info script]]]
+set diffusion_coefficient_dir [file dirname [file normalize [info script]]]
 
 # source [file join $diffusion_coefficient_gui::SCRIPTDIR diffusion_coefficient_gui_ui.tcl]
 
-puts "diffusion_coefficient) Adding directory $here to auto_path and registering the menu"
+puts "diffusion_coefficient) Adding directory $diffusion_coefficient_dir to auto_path and registering the menu"
 
-lappend auto_path $here
+lappend auto_path $diffusion_coefficient_dir
 package require diffusion_coefficient_gui
 ::diffusion_coefficient_gui::register_menu
 
